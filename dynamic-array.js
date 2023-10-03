fd2a05a5826d5365cd8caf304bcd3ca4cfa1cd9c
Erich <erichnguyen25@gmail.com>
2023-10-03 16:25:52 -0500

    Update dynamic-array.js

--- dynamic-array.js
+++ dynamic-array.js
@@ -1,46 +1,54 @@
 class DynamicArray {
 
   constructor(defaultSize = 4) {
-
-    // Your code here 
+    this.capacity = defaultSize
+    this.length = 0
+    this.data = new Array(defaultSize)
   }
 
   read(index) {
-
-    // Your code here 
+    return this.data[index]
   }
 
   push(val) {
-
-    // Your code here 
+    if(this.length===this.capacity) this.resize()
+    this.data[this.length] = val
+    this.length++
   }
-
 
   pop() {
-
-    // Your code here 
+    if(!this.length) return undefined
+    
+    const value = this.data[this.length-1]
+    this.data[--this.length] = undefined
+    return value
   }
-
+  
   shift() {
+    if(!this.length) return undefined
 
-    // Your code here 
+    const value = this.data[0]
+    for(let i=1;i<this.length;i++) this.data[i-1] = this.data[i]
+    this.length--
+    return value
   }
 
   unshift(val) {
-
-    // Your code here 
+    if(this.length===this.capacity) this.resize()
+    for(let i=this.length;i>0;i--) this.data[i] = this.data[i-1]
+    this.data[0] = val  
+    this.length++
   }
 
   indexOf(val) {
-
-    // Your code here 
+    for(let i=0;i<this.length;i++) if(this.data[i]===val) return i
+    return -1
   }
 
   resize() {
-
-    // Your code here 
+    this.data = [...this.data, ...new Array(this.capacity)]
+    this.capacity *= 2
   }
-
 }
 
 
